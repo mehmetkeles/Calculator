12c25eeb9b3cda9482015bcbf16bb389399bd5eb
mehmetkeles <46766283+mehmetkeles@users.noreply.github.com>
2019-01-19 15:55:16 -0600

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -12,7 +12,7 @@ public class Calculator {
     return num1*num2;
     }
     
-    public static divide(double num1,double num2){
+    public static double divide(double num1,double num2){
     return num1/num2; 
     }
 }
